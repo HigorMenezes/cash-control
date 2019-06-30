c7c2aeac1071ea54d80834c49eb2eaf81fdd205e
Higor Menezes <higor_menezes96@hotmail.com.br>
2019-06-30 14:24:51 -0400

    fix heroku port

--- src/index.js
+++ src/index.js
@@ -2,33 +2,8 @@ const app = require('./app');
 const appConfig = require('./configs/appConfig');
 const logger = require('./utils/winstonUtil')('info', 'index');
 
-app.listen(appConfig.port, () => {
+app.listen(process.env.PORT || appConfig.port, () => {
   logger.info(
     `Server is running on port: ${appConfig.port} and env: ${appConfig.env}`,
   );
 });
-
-// const { CashFlow } = require('./app/models');
-
-// const findAllCash = async (userId, type) => {
-//   try {
-//     const queryResult = await CashFlow.findAll({
-//       where: { type, userId },
-//       raw: true,
-//     });
-//     let result = { value: 0.0 };
-//     if (queryResult.length > 0) {
-//       result = queryResult.reduce(({ value }, current) => {
-//         return { value: value + current.value };
-//       });
-//     }
-//     return result.value;
-//   } catch (err) {
-//     logger.error('findAllCash - Error during find all CashIn', err);
-//     throw err;
-//   }
-// };
-
-// findAllCash(4, 'cash-out')
-//   .then(console.log)
-//   .catch(console.log);
